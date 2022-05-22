6595fbc478bdc23fc1b1d92f37059d72c3ea2853
Junwei Hu <junweihu@microsoft.com>
2022-05-22 18:36:19 +0800

    Working version

---
+++ order-api/src/main/java/com/microservices/orderapi/OrderApiApplication.java
@@ -0,0 +1,14 @@
+package com.microservices.orderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApiApplication.class, args);
+    }
+
+}
